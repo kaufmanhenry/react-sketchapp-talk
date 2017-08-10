fc1fd03ebfbbc6527c4d95acfe46f030f297b4a3
Henry Kaufman <hkaufman19@gmail.com>
2017-08-09 19:26:04 -0600

    :flashlight: Added drop shadow on the dog card

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -8,7 +8,11 @@ const styles = StyleSheet.create({
     color: '#111',
     width: 800,
     margin: 20,
-    borderRadius: 4
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.05,
+    shadowRadius: 20
   },
   container: {
     padding: 20
